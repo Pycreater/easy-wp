c134fdd89ddcf6d510e865d81a1a7decc1146022
Pratik <pratikyadav3949@gmail.com>
2025-03-30 20:08:44 +0530

    created the update quantity func

--- providers/cart-context.tsx
+++ providers/cart-context.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -14,11 +14,63 @@ interface CartItem {
   images: { src: string }[];
 }
 
+interface CartContextType {
+  items: CartItem[];
+  addItem: (product: any) => void;
+  removeItem: (productId: string) => void;
+  updateQuantity: (productId: string, quantity: number) => void;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  cartTotal: number;
+}
+
+const CartContext = React.createContext<CartContextType | undefined>(undefined);
+
 const CartProvider = ({ children }: Props) => {
   const [items, setItems] = useState<CartItem[]>([]);
   const [isOpen, setIsOpen] = useState(false);
 
-  return <div>{children}</div>;
+  const addItem = useCallback((product: any) => {
+    setItems((prevItems) => {
+      const existingItem = prevItems.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        return prevItems.map((item) =>
+          item.id === product.id
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        );
+      }
+      return [...prevItems, { ...product, quantity: 1 }];
+    });
+    setIsOpen(true);
+  }, []);
+
+  const removeItem = useCallback((productId: string) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id != productId));
+  }, []);
+
+  const updateQuantity = useCallback((productId: string, quantity: number) => {
+    if (quantity < 1) {
+      removeItem(productId);
+      return;
+    }
+
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === productId ? { ...item, quantity } : item
+      )
+    );
+  }, []);
+
+  return (
+    <CartContext.Provider
+      value={{ items, addItem, removeItem }}
+    ></CartContext.Provider>
+  );
 };
 
 export default CartProvider;
