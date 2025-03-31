5d8d3a300bb3c8bfe41d9c4107954d484d1b67c3
Pratik <pratikyadav3949@gmail.com>
2025-03-31 19:28:25 +0530

    cart functionality chk-1

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,7 +1,17 @@
 "use client";
 
-import { MoreVertical, X } from "lucide-react";
+import { navbarData } from "@/lib/data";
+import { MoreVertical, ShoppingCart, X } from "lucide-react";
+import Link from "next/link";
 import React, { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { useCart } from "@/hooks/use-cart";
 
 type Props = {
   products: any[];
@@ -9,6 +19,7 @@ type Props = {
 
 const ProductGrid = ({ products }: Props) => {
   const [toggle, setToggle] = useState(false);
+  const { isOpen, setIsOpen, items: cartItems } = useCart();
 
   return (
     <div className="min-h-screen bg-white">
@@ -24,9 +35,42 @@ const ProductGrid = ({ products }: Props) => {
           </button>
           {toggle && (
             <div className="w-full flex gap-x-10 items-center justify-center">
-              here im currently
+              {navbarData.map((item) => (
+                <Link key={item.idx} href={item.link}>
+                  {item.title}
+                </Link>
+              ))}
             </div>
           )}
+
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <button className="text-lg font-medium flex items-center gap-2 hover:cursor-pointer">
+                <span>Cart</span>
+                {cartItems.length > 0 && (
+                  <span className="inline-flex items-center justify-center w-5 h-5 text-xs font-medium bg-black text-white rounded-full">
+                    {cartItems.reduce(
+                      (total, item) => total + item.quantity,
+                      0
+                    )}
+                  </span>
+                )}
+              </button>
+            </SheetTrigger>
+            <SheetContent className="w-full sm:max-w-md">
+              <SheetHeader>
+                <SheetTitle>Your Cart</SheetTitle>
+              </SheetHeader>
+              {cartItems.length === 0 ? (
+                <div className="flex flex-col items-center justify-center h-[50vh]">
+                  <ShoppingCart className="h-12 w-12 text-gray-300 mb-4" />
+                  <p className="text-gray-500">Your cart is empty</p>
+                </div>
+              ) : (
+                ""
+              )}
+            </SheetContent>
+          </Sheet>
         </div>
       </header>
     </div>
