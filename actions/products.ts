c0b72342ed30ab6c539f00167c1da33cded37ef6
Pratik <pratikyadav3949@gmail.com>
2025-04-03 08:55:53 +0530

    feat: get single product info by id dynamically

--- actions/products.ts
+++ actions/products.ts
@@ -13,3 +13,9 @@ export const getProducts = async () => {
   const products = await woocommerce.get("products");
   return products.data;
 };
+
+export const getProduct = async (id: string) => {
+  const product = await woocommerce.get(`products`, { id: parseInt(id) });
+
+  return product.data;
+};
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,10 +1,13 @@
+import { getProduct } from "@/actions/products";
 import React from "react";
 
 type Props = {
   params: { id: string };
 };
 
-const ProductPage = ({ params: { id } }: Props) => {
+const ProductPage = async ({ params: { id } }: Props) => {
+  const product = await getProduct(id);
+
   return <div>{id}</div>;
 };
 
