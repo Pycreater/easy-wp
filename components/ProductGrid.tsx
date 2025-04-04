19efff609c190bd695cb736426e00f4f90c8fc69
Pratik <pratikyadav3949@gmail.com>
2025-04-04 20:40:31 +0530

    f

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -136,7 +136,7 @@ const ProductGrid = ({ products = [] }: Props) => {
                       <p>Subtotal</p>
                       <p>${cartTotal}</p>
                     </div>
-                    {/* wip: <Checkout /> */}
+                    {/* wip: <Checkout /> */} hfd
                   </div>
                 </div>
               )}
